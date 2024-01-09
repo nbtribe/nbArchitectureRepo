7321b7542427a0c9ed936d8db06959de8ef6a52a
NB <trybe.endeavors@gmail.com>
2024-01-09 16:49:30 -0500

    sFTP infrastructure deploy project

--- sftp_infra_automated_deployment/sftp_infra_automated_deployment/sftp_infra_automated_deployment_stack.py
+++ sftp_infra_automated_deployment/sftp_infra_automated_deployment/sftp_infra_automated_deployment_stack.py
@@ -1,19 +1,259 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    # aws_sqs as sqs,
+    CfnParameter,
+    aws_s3 as s3,
+    aws_s3_deployment as s3deploy,
+    aws_s3_notifications as s3n,
+    aws_lambda as lambda_,
+    aws_iam as iam,
+    aws_secretsmanager as secretsmanager,
+    SecretValue,
+    Duration,
+    RemovalPolicy
 )
 from constructs import Construct
+
+import os
+import json
 
 class SftpInfraAutomatedDeploymentStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+                # Create a CfnParameter for the client acronym
+        client_acromym = CfnParameter(self,"ClientAcronym",
+                                      type="String",
+                                      description="Enter the client acronym",)
+        # source_system = CfnParameter(self,"SourceSystem",
+        #                               type="String",
+        #                               description="Enter the source system",)
+        # module = CfnParameter(self,"Module",
+        #                               type="String",
+        #                               description="Enter the module",)
+        # destination_bucket = CfnParameter(self,"DestinationBucket",
+        #                                   type="String",
+        #                                   description="Enter the destination bucket",)
+        # source_bucket = CfnParameter(self,"SourceBucket",
+        #                                   type="String",
+        #                                   description="Enter the source bucket",)
+        # destination_prefix = CfnParameter(self,"DestinationPrefix",
+        #                                   type="String",
+        #                                   description="Enter the destination prefix",)
+        # source_prefix = CfnParameter(self,"SourcePrefix",
+        #                                   type="String",
+        #                                   description="Enter the source prefix",)
+        
+        
+        x_client_acronym = f"{client_acromym.value_as_string}"
+        self.x_client_acronym = self.node.try_get_context('x_client_acronym')
+        # x_source_system = f"{source_system.value_as_string}"
+        # x_module = f"{module.value_as_string}"
+        # x_destination_bucket = f"{destination_bucket.value_as_string}"
+        # x_source_bucket = f"{source_bucket.value_as_string}"
+        # x_destination_prefix = f"{destination_prefix.value_as_string}"
+        # x_source_prefix = f"{source_prefix.value_as_string}"
+        
+        
+        # Fill Out the below params
+        SOURCE_SYS = "oas"
+        MODULE = "lms"
+        DESTINATION_BUCKET ="case-dm-associationanalytics"
+        SOURCE_BUCKET = "a2-ftp"
+        customer = "case"
+        REQUIRED_FILES = "CurrentCourses.csv;UserEnrollments.csv;CreditClaimed.csv;Learners.csv;CourseFeedback.csv;CertifiedUsers.csv;InstructorRatings.csv;CoursePerformance.csv;CourseInTracks.csv;Tracks.csv"
+        DEST_PREFIX	= f"data/{MODULE}_{SOURCE_SYS}"
+        SOURCE_PREFIX = f"{x_client_acronym}_{SOURCE_SYS}"
+        SOURCE_PREFIX_PATH= f"{x_client_acronym}_{SOURCE_SYS}/toprocess"
+        
+        
+        
 
-        # The code that defines your stack goes here
+        
+        # existing bucket
+        bucket = s3.Bucket.from_bucket_attributes(self, "ExistingBucket",
+        bucket_arn = f"arn:aws:s3:::{SOURCE_BUCKET}", 
+        )
+        
+        s3deploy.BucketDeployment(self, "DeployFileStruct",
+                                  sources=[s3deploy.Source.asset("assets")],
+                                  destination_bucket=bucket,
+                                #   destination_key_prefix="ttest_sftp/toprocess",
+                                  destination_key_prefix=SOURCE_PREFIX_PATH,
+        )
+                                  
+        
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "SftpInfraAutomatedDeploymentQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # lambda role
+        lambda_role = iam.Role(self, "LambdaSFTPRole",
+                                role_name= f"{customer}_{SOURCE_SYS}_SFTPRole",
+                                assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
+                                managed_policies=[iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSLambdaBasicExecutionRole"),
+                                                  ],
+                                )
+        
+        lambda_role.attach_inline_policy(
+                            iam.Policy(self, f"{customer}-{SOURCE_SYS}-lambda-SFTP-policy",
+                    
+                                        statements=[
+                                            iam.PolicyStatement(
+                                                effect=iam.Effect.ALLOW,
+                                                actions=["s3:DeleteObject","s3:GetObject"],
+                                                # resources=[f"arn:aws:s3:::{SOURCE_BUCKET}/{x_client_acronym}_{SOURCE_SYS}/toprocess/*"]
+                                                resources=[f"arn:aws:s3:::{SOURCE_BUCKET}/{SOURCE_PREFIX_PATH}/*"]
+                                    ),
+                                            iam.PolicyStatement(
+                                                effect=iam.Effect.ALLOW,
+                                                actions=["s3:ListBucket"],
+                                                resources=[f"arn:aws:s3:::{SOURCE_BUCKET}",
+                                                            # f"arn:aws:s3:::{SOURCE_BUCKET}/{x_client_acronym}_{SOURCE_SYS}/toprocess*"]
+                                                            f"arn:aws:s3:::{SOURCE_BUCKET}/{SOURCE_PREFIX}*"]
+                                    ),
+                                            iam.PolicyStatement(
+                                                effect=iam.Effect.ALLOW,
+                                                actions=["s3:PutObject"],
+                                                # resources=[f"arn:aws:s3:::{DESTINATION_BUCKET}/data/{SOURCE_SYS}*"]
+                                                resources=[f"arn:aws:s3:::{DESTINATION_BUCKET}/{DEST_PREFIX}*"]
+                                    ),
+                                            iam.PolicyStatement(
+                                                effect=iam.Effect.ALLOW,
+                                                actions=["s3:PutObject","s3:GetObject"],
+                                                resources=[f"arn:aws:s3:::{DESTINATION_BUCKET}/scripts/*"]
+                                    )
+                                    ]
+       ))
+                                                   
+        
+        # Create lambda function
+        sftp_lambda = lambda_.Function(self, "SFTPProcessFilesAuto",
+                                       function_name=f"{x_client_acronym}-{SOURCE_SYS}-SFTPProcessFilesAuto",
+                                        runtime=lambda_.Runtime.PYTHON_3_10,
+                                        handler="process_files.lambda_handler",
+                                        code=lambda_.Code.from_asset(f"src/lambda/{SOURCE_SYS}"),
+                                        role=lambda_role,
+                                        timeout=Duration.seconds(300),
+                                        memory_size=1024,
+                                        environment={"DESTINATION_BUCKET": DESTINATION_BUCKET,
+                                                     "DEST_PREFIX" : DEST_PREFIX,
+                                                     "CUSTOMER": f"{x_client_acronym}",
+                                                     "SOURCE_BUCKET": SOURCE_BUCKET,
+                                                     "SOURCE_PREFIX": SOURCE_PREFIX_PATH,
+                                                     "SOURCE_SYS": f"{MODULE}_{SOURCE_SYS}",
+                                                     "REQUIRED_FILES": REQUIRED_FILES
+                                                     },
+                                        
+                                       
+        )
+        # Event Bucket Notification fires the processing lambda
+        bucket.add_event_notification(s3.EventType.OBJECT_CREATED,
+                                      s3n.LambdaDestination(sftp_lambda),
+                                    s3.NotificationKeyFilter(prefix=f"{SOURCE_PREFIX_PATH}/"),
+                                      s3.NotificationKeyFilter(suffix=".csv")
+                                      )
+
+
+        # Create the Secret in Secrets Manager
+        sftp_secret = secretsmanager.Secret(self, "Secret",
+                                            secret_name=f"SFTP/{x_client_acronym}_{SOURCE_SYS}",
+                                            description=f"SFTP credentials for {x_client_acronym} {MODULE}_{SOURCE_SYS}",
+                                            removal_policy=RemovalPolicy.DESTROY,
+                                            generate_secret_string=secretsmanager.SecretStringGenerator(
+                                                secret_string_template= json.dumps({"Role":f"arn:aws:iam::009885916296:role/a2_sftp_{x_client_acronym}_{MODULE}_{SOURCE_SYS}_role","HomeDirectory":f"/{SOURCE_BUCKET}/{x_client_acronym}_{SOURCE_SYS}","PublicKey1":""}),
+                                                generate_string_key='Password',
+                                                include_space=False
+                                            )
+                                            )
+        
+        
+        
+        # Probably have to create the Role that Allows Transfer Family to call AWS services here
+        transfer_fam_role = iam.Role(self, "Transfer Family Role",
+                                role_name = f"a2_sftp_{x_client_acronym}_{MODULE}_{SOURCE_SYS}_role",
+                                
+                                assumed_by=iam.ServicePrincipal("transfer.amazonaws.com"),
+
+                                )
+    
+        # Create the trasfer family policy 
+        transfer_policy = iam.Policy(self, f"{customer}-{SOURCE_SYS}-TransferPolicy",
+                                     
+        )
+        # add to the policy
+        transfer_policy.add_statements(
+            iam.PolicyStatement(
+                sid = "AllowListingOfUserFolder",
+                effect=iam.Effect.ALLOW,
+                actions=["s3:ListBucket"],
+                resources=[f"arn:aws:s3:::{SOURCE_BUCKET}"],
+                conditions={
+                    "StringLike": {
+                        "s3:prefix": [f"{SOURCE_PREFIX}/*",
+                                      f"{SOURCE_PREFIX}"]
+                    }}
+                
+            )
+        )
+        # add to the policy
+        transfer_policy.add_statements(
+            iam.PolicyStatement(
+                sid = "HomeDirObjectAccess",
+                effect=iam.Effect.ALLOW,
+                actions=["s3:PutObject",
+                         "s3:GetObject",
+                         "s3:DeleteObjectVersion",
+                         "s3:DeleteObject",
+                         "s3:GetObjectVersion"],
+                resources=[f"arn:aws:s3:::{SOURCE_BUCKET}/{SOURCE_PREFIX}*"],
+                # conditions={
+                #     "StringLike": {
+                #         "s3:prefix": [f"{SOURCE_PREFIX}/*",
+                #                       f"{SOURCE_PREFIX}"]
+                #     }}
+                
+            )
+        )
+        
+        # Attach Transfer policy to the Transfer Family role
+        transfer_fam_role.attach_inline_policy(transfer_policy)
+        
+        
+        # ^^THIS HAS TO ATTACH DIRECTKY TO THE Access USER
+                # existing user
+        existing_user = iam.User.from_user_attributes(self, "ImportedUser",
+                        # user_arn=f"arn:aws:iam::009885916296:user/{x_client_acronym}AccessUser",
+                        user_arn = "arn:aws:iam::009885916296:user/CASEExtractProgram"
+        )
+        # Attaching policy to existing Access User
+        transfer_policy.attach_to_user(existing_user)
+
+        
+    #     existing_user.attach_inline_policy(
+    #                         iam.Policy(self, f"a2_SFTP_{client_acromym}_policy",
+                                    
+    #                                     statements=[
+    #                                         iam.PolicyStatement(
+    #                                             # sid = "AllowListingOfUserFolder",
+    #                                             effect=iam.Effect.ALLOW,
+    #                                             actions=["s3:ListBucket"],
+    #                                             resources=[f"arn:aws:s3:::{SOURCE_BUCKET}"],
+    #                                 ),
+    #                                         iam.PolicyStatement(
+    #                                             effect=iam.Effect.ALLOW,
+    #                                             actions=["s3:ListBucket"],
+    #                                             resources=[f"arn:aws:s3:::{SOURCE_BUCKET}",
+    #                                                         # f"arn:aws:s3:::{SOURCE_BUCKET}/{x_client_acronym}_{SOURCE_SYS}/toprocess*"]
+    #                                                         f"arn:aws:s3:::{SOURCE_BUCKET}/{SOURCE_PREFIX}*"]
+    #                                 ),
+    #                                         iam.PolicyStatement(
+    #                                             effect=iam.Effect.ALLOW,
+    #                                             actions=["s3:PutObject"],
+    #                                             # resources=[f"arn:aws:s3:::{DESTINATION_BUCKET}/data/{SOURCE_SYS}*"]
+    #                                             resources=[f"arn:aws:s3:::{DESTINATION_BUCKET}/{DEST_PREFIX}*"]
+    #                                 ),
+    #                                         iam.PolicyStatement(
+    #                                             effect=iam.Effect.ALLOW,
+    #                                             actions=["s3:PutObject","s3:GetObject"],
+    #                                             resources=[f"arn:aws:s3:::{DESTINATION_BUCKET}/scripts/*"]
+    #                                 )
+    #                                 ]
+    #    ))
---
+++ sftp_infra_automated_deployment/src/lambda/oas/process_files.py
@@ -0,0 +1,154 @@
+import boto3
+import os
+from datetime import datetime 
+import sys
+import json
+import io
+import gzip
+
+# OASIS SFTP LAMBDA
+# Get the current date in YYYYMMDD format
+current_date = datetime.now().strftime("%Y%m%d")
+source_bucket = os.environ["SOURCE_BUCKET"]
+destination_bucket = os.environ["DESTINATION_BUCKET"]
+destination_prefix = os.environ["DEST_PREFIX"]
+source_prefix = os.environ["SOURCE_PREFIX"]
+customer = os.environ["CUSTOMER"]
+source_sys = os.environ["SOURCE_SYS"]
+required_files = os.environ["REQUIRED_FILES"]
+required_files_list = list(required_files.split(";"))
+s3 = boto3.client('s3')
+print(required_files_list)
+
+def check_required_files():
+    # Create an S3 client
+    s3 = boto3.client('s3')
+
+    # Get the list of files in the source bucket
+    response = s3.list_objects_v2(Bucket=source_bucket, Prefix=source_prefix)
+    files = [obj['Key'] for obj in response.get('Contents', [])]
+
+    # Check if all required files are present
+    for file in required_files_list:
+        if file not in files:
+            print("All files not present")
+            return False
+
+    return True
+def update_copy_script():
+    template_key = f'{customer.lower()}_{source_sys}_copy_template.sql'
+    scripts_folder = 'scripts/'
+    print(f"template_key = {template_key}")
+
+    try:
+        # Get the template file content from S3
+        response = s3.get_object(Bucket=destination_bucket, Key=f"{scripts_folder}{template_key}")
+        template_content = response['Body'].read().decode('utf-8')
+
+        # Replace the placeholder with the current date
+        current_date = datetime.now().strftime('%Y%m%d')
+        new_content = template_content.replace('{datefolder}', current_date)
+
+        # # Create a new file key for the destination
+        new_file_key= f"{scripts_folder}{customer}_{source_sys}_copy.sql"
+
+        # Upload the new content to the destination in S3
+        s3.put_object(Body=new_content.encode('utf-8'), Bucket=destination_bucket, Key=new_file_key)
+
+        print(f"New file created and uploaded to: s3://{destination_bucket}/{new_file_key}")
+
+        return {
+            'statusCode': 200,
+            'body': 'New file created and uploaded successfully'
+        }
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return {
+            'statusCode': 500,
+            'body': 'Error creating or uploading the new file'
+        }
+        
+ # Oasis specific- they send a file like CertifiedUsers - 03-29-2023.csv
+def clean_filename(filename):
+    # Replace all non-alphanumeric characters with underscores
+    filename = f"{filename.split('-')[0].strip()}.csv"
+    return filename
+
+def move_files(source_bucket, source_prefix, destination_bucket, destination_prefix):
+    
+    # List all objects in the source path
+    response = s3.list_objects_v2(Bucket=source_bucket, Prefix=source_prefix)
+
+
+    # Iterate through each object in the source path
+    for obj in response.get('Contents', []):
+        # Construct the source and destination object keys
+        source_key = obj['Key']
+        source_key_file = f"{source_key.split('/')[2]}"
+        
+        # Oasis specific- they send a file like CertifiedUsers - 03-29-2023.csv
+        
+        source_key_file = clean_filename(source_key_file)
+        
+        destination_key = source_key.replace(source_prefix, f"{destination_prefix}/{current_date}", 1)
+        print(f"DESTINATIONKEY= {destination_key}")
+        
+
+        # Get the file content from S3
+        response = s3.get_object(Bucket=source_bucket, Key=source_key)
+        file_content = response['Body'].read()
+        
+        # Gzip the file content
+        compressed_content = gzip.compress(file_content)
+        
+        # Upload the gzipped content to the new S3 key
+        # if len(source_key_file) > 0 and source_key_file !=".keep":
+        if source_key_file in required_files_list:
+            s3.put_object(Body=compressed_content, Bucket=destination_bucket, Key=f"{destination_key}.gz", ContentEncoding='gzip')
+        else: 
+            continue
+
+
+        # # Copy the object to the new location
+        # s3.copy_object(
+        #     Bucket=destination_bucket ,
+        #     CopySource={'Bucket': source_bucket, 'Key': source_key},
+        #     Key=destination_key
+        # )
+        if len(source_key_file) > 0 and source_key_file in required_files_list:
+            # Delete the original object from the source path
+            # os.remove(f"/{source_key}")
+            s3.delete_object(Bucket=source_bucket, Key=source_key)
+            print(f"Deleted: s3://{source_bucket}/{source_key}")
+
+            print(f"Uploaded gzipped file to: s3://{destination_bucket}/{destination_key}")
+
+        else:
+            continue
+    
+    # s3.put_object(Body=compressed_content, Bucket=destination_bucket, Key=f"{destination_prefix}/{current_date}/eor_{source_sys}.csv.gz", ContentEncoding='gzip')
+    # print("eor uploaded")
+def upload_eor():
+    compressed_content = ""
+    s3.put_object(Body=compressed_content, Bucket=destination_bucket, Key=f"{destination_prefix}/{current_date}/eor_{source_sys}.csv.gz", ContentEncoding='gzip')
+    print("eor uploaded")
+    
+def lambda_handler(event, context):
+    if check_required_files() is False:
+        return {
+            'statusCode': 500,
+            'body': 'ALL Required files not found in source bucket'
+        }
+    else:
+        # Move files from source to destination
+        move_files(source_bucket, source_prefix, destination_bucket, destination_prefix)
+        # Upload EOR
+        upload_eor()
+        # Update Copy script 
+        update_copy_script()
+
+
+        return {
+            'statusCode': 200,
+            'body': 'Files moved successfully!'
+        }
